cba53b7e5c24b69eb46bd230db26171cb4886258
linjw <10126121@qq.com>
2026-06-30 23:49:23 +0800

    添加v佬hp

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -446,7 +446,7 @@ apply_iwrt_package_overrides() {
     UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
     UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
-    # UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+    UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
     UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
     UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
     # UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
