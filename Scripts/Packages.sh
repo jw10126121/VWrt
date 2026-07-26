ecc72e4ee07ea181d8ef115b8fa5a33d77b4568c
linjw <10126121@qq.com>
2026-07-26 23:17:47 +0800

    升级hp

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -604,6 +604,8 @@ apply_iwrt_package_overrides() {
     # update_package_list "luci-app-quickstart quickstart" "sbwml/package_new_istore" "main"
     # update_package_list "luci-app-homeproxy sing-box" "VIKINGYFY/packages" "main"
     # UPDATE_VERSION "sing-box" # 升级sing-box到最新release版本，如果使用VIKINGYFY/packages/luci-app-homeproxy，要求sing-box版本>=1.14.0，否则luci-app-homeproxy无法编译成功
+    update_package_list "luci-app-homeproxy sing-box" "XiaoHaiSly/luci-app-homeproxy2" "main"
+
 }
 
 # OpenWrt 25.12 的 LuCI 菜单机制与语言包状态和旧分支不同，这里统一补一层兼容：
