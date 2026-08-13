772239cdddb83081f448034bace7bece52b31a7a
linjw <10126121@qq.com>
2026-08-13 21:59:22 +0800

    添加substore源

--- Scripts/diy_after_defconfig.sh
+++ Scripts/diy_after_defconfig.sh
@@ -270,3 +270,18 @@ SOURCE_TYPE=$(resolve_source_type "${SOURCE_TYPE:-auto}" "${openwrt_workdir}")
 if [ "${SOURCE_TYPE}" = "lean" ]; then
     configure_ecm_accel_delay_fix
 fi
+
+
+#写入自定义APK软件源及签名公钥
+mkdir -p ./files/etc/apk/keys ./files/etc/apk/repositories.d
+
+cat > ./files/etc/apk/repositories.d/substore.list <<EOF
+https://substore-openwrt.pages.dev/openwrt-25.12/all/packages.adb
+EOF
+
+cat > ./files/etc/apk/keys/substore-apk.pem <<'EOF'
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEJKvnnTePdD16sK/rksork3HzOxeQ
+YJjfM7/Fd1eVSpC7k4I/80OpF8lxuoCMbNilssnMtG2WUv/idDjcIEa+Lw==
+-----END PUBLIC KEY-----
+EOF
